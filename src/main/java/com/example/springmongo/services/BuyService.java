fcf810303b169ab9dfa6af839be16e10735d1803
Oscar <oscar.farfan@sofka.com.co>
2023-07-27 12:31:45 -0500

    Se realiza ajuste en las pruebas del servicio de compras

--- src/main/java/com/example/springmongo/services/BuyService.java
+++ src/main/java/com/example/springmongo/services/BuyService.java
@@ -4,6 +4,7 @@ import com.example.springmongo.models.BuyModel;
 import com.example.springmongo.models.Order;
 import com.example.springmongo.models.ProductModel;
 import com.example.springmongo.repositories.BuyRepository;
+import com.example.springmongo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ public class BuyService {
     BuyRepository buyRepository;
     @Autowired
     ProductService productService;
+    @Autowired
+    ProductRepository productRepository;
 
     public BuyModel saveBuy(BuyModel buyModel){
         return buyRepository.save(buyModel);
@@ -29,7 +32,7 @@ public class BuyService {
         ArrayList<Order> products = buyModel.getProducts();
         boolean validation = true;
         for (Order product: products) {
-            ProductModel productModel = productService.getProductById(product.getIdProduct());
+            ProductModel productModel = productRepository.findById(product.getIdProduct()).get();
             if (productModel.getInInventory() < product.getQuantity()){
                 validation = false;
             }
@@ -41,7 +44,7 @@ public class BuyService {
         ArrayList<Order> products = buyModel.getProducts();
         boolean validation = true;
         for (Order product: products) {
-            ProductModel productModel = productService.getProductById(product.getIdProduct());
+            ProductModel productModel = productRepository.findById(product.getIdProduct()).get();
             if (product.getQuantity() < productModel.getMin()){
                 validation = false;
             }
@@ -53,7 +56,7 @@ public class BuyService {
         ArrayList<Order> products = buyModel.getProducts();
         boolean validation = true;
         for (Order product: products) {
-            ProductModel productModel = productService.getProductById(product.getIdProduct());
+            ProductModel productModel = productRepository.findById(product.getIdProduct()).get();
             if (product.getQuantity() > productModel.getMax()){
                 validation = false;
             }
@@ -64,7 +67,7 @@ public class BuyService {
     public void settlePurchase(BuyModel buyModel){
         ArrayList<Order> products = buyModel.getProducts();
         for (Order product: products) {
-            ProductModel productModel = productService.getProductById(product.getIdProduct());
+            ProductModel productModel = productRepository.findById(product.getIdProduct()).get();
             productModel.setInInventory(productModel.getInInventory() - product.getQuantity());
             productService.updateProduct(productModel);
         }
